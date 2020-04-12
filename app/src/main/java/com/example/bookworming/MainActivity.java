7a82ab9a5d02933bd6bd720b3a9cd25ca1a2bf9b
spiroskarathanassis <karathanassis13@gmail.com>
2020-04-13 01:57:29 +0300

    two screens

--- app/src/main/java/com/example/bookworming/MainActivity.java
+++ app/src/main/java/com/example/bookworming/MainActivity.java
@@ -2,18 +2,37 @@ package com.example.bookworming;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.AdminLibraryManagement;
 import com.example.bookworming.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.onClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLibraryManagementActivity();
+            }
+        });
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+
+
+    }
+
+    public void openLibraryManagementActivity() {
+        Intent intent = new Intent(this, AdminLibraryManagement.class);
+        startActivity(intent);
     }
 }
