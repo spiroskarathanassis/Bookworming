dded6b730b1f0567cdcbb2b3181de06871b9e4b9
spiroskarathanassis <karathanassis13@gmail.com>
2020-04-12 20:25:49 +0300

    Inital commit

---
+++ app/src/main/java/com/example/bookworming/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.bookworming;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.bookworming.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
