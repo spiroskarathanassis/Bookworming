8a9044dc738a389d0386d5f11dc5c94635e20ca9
GiorgosTzelepis <geo335127@gmail.com>
2020-05-15 19:45:44 +0300

    new commit

--- app/src/main/java/com/example/bookworms/MainActivity.java
+++ app/src/main/java/com/example/bookworms/MainActivity.java
@@ -9,10 +9,16 @@ import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
 import androidx.appcompat.app.AppCompatActivity;
+import java.io.File;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Book book1 = new Book("book", "author", 2125);
+
+
 
     ImageButton imageButton;
 
@@ -22,10 +28,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.arxiki_selida);
 
         addListenerOnButton();
-
+        //printMessageOlok();
+        //printMessageError()
     }
 
-
+    //methods...
+    //button listener
     public void addListenerOnButton() {
 
         imageButton = (ImageButton)findViewById(R.id.newbutton);
@@ -36,11 +44,171 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View arg0) {
 
                 Toast.makeText(MainActivity.this,
-                        "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+                        book1.title, Toast.LENGTH_SHORT).show();
 
             }
 
         });
+    }
+
+    //ektiposi minimatos oloklirosis doreas
+    public void printMessageOlok() {
+
+        imageButton = (ImageButton)findViewById(R.id.newbutton);
+
+        imageButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(MainActivity.this,
+                        "Η διαδικασία της δωρεάς ολοκληρώθηκε με επιτχία!",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+    }
+
+    //ektiposi minimatos lathous
+    public void printMessageError() {
+
+        imageButton = (ImageButton)findViewById(R.id.newbutton);
+
+        imageButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(MainActivity.this,
+                        "Τα στοιχεία σας είναι λάθος.", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+    }
+
+}
+
+
+
+class Book{
+
+    String title;
+    String author;
+    int isbn;
+
+    public Book(String ttl, String aut, int is){
+
+        title = ttl;
+        author = aut;
+        isbn = is;
+        }
+    //elegxos stoixeiwn pou edwse o xristis
+    //public void checkBookData(){
+
+    //}
+
+
+}
+
+
+//class Author{
+
+    //public Author(){
+
+   // }
+//}
+
+//class Publisher{
+
+//public Publisher(){
+
+// }
+//}
+
+
+class User{
+    String name;
+    String email;
+    String password;
+    int points;
+    int id;
+
+    public User(String na, String em, String pass){
+        name = na;
+        email = em;
+        password = pass;
+    }
+
+    public boolean bookCheck(){
+
+        if(true) {
+
+            checkDate();
+            return true;//findBook()
+        }
+        else {
+
+            return false;//notFindBook()
+        }
+    }
+
+    public boolean checkDate(){
+
+        if(true) {
+
+            return true;//findBookNotXreosi()
+        }
+        else {
+
+            return false;//FindBookXreosi()
+        }
+
+    }
+
+    public void addPoints(int newpoints){
+
+        points = points + newpoints;
+    }
+}
+
+
+
+class Library{
+    int id;
+    String name;
+
+    public Library(int i_d, String na){
+        id = i_d;
+        name = na;
+
+    }
+}
+
+
+class Coupons{
+    int id;
+    float discount;
+    String name;
+
+    public Coupons(int i_d, float disc, String na){
+        id = i_d;
+        discount = disc;
+        name = na;
+    }
+}
+
+
+class BookWorming{
+    int id;
+    String type;
+    String title;
+
+    public BookWorming(int i_d, String ty, String tit){
+        id = i_d;
+        type = ty;
+        title = tit;
 
     }
 }
