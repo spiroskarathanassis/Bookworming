b156306938a94c6c4b75ec6a768781701587a54e
georgioynikh <65353848+georgioynikh@users.noreply.github.com>
2020-05-27 17:11:48 +0300

    availability list

---
+++ app/src/main/java/com/example/Bookworming/AvailabilityList.java
@@ -0,0 +1,48 @@
+package com.example.bookworms;
+
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AvailabilityList{
+
+    int user_id;
+    int book_id;
+    //int availability;
+
+    public AvailabilityList(int uid, int bid){
+
+        user_id = uid;
+        book_id = bid;
+    }
+
+    public void  add_availabilitylist(){
+
+        File file = new File("AvailabilityList.csv");
+        try {
+            // create FileWriter object with file as parameter
+            FileWriter outputfile = new FileWriter(file);
+
+            // create CSVWriter object filewriter object as parameter
+            CSVWriter writer = new CSVWriter(outputfile);
+
+            // adding header to csv
+            String[] header = {"user_id", "book_id"};
+            writer.writeNext(header);
+
+            // add data to csv
+            String[] data1 = {"1", "10"};
+            writer.writeNext(data1);
+            String[] data2 = {"1", "10"};;
+            writer.writeNext(data2);
+
+            // closing writer connection
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
