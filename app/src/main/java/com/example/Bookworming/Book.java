3a138350b12da1598c4143e7594b32e78ac0fd69
GiorgosTzelepis <geo335127@gmail.com>
2020-05-27 22:37:52 +0300

    new class commit

---
+++ app/src/main/java/com/example/Bookworming/Book.java
@@ -0,0 +1,137 @@
+package com.example.bookworms;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book{
+
+    int id;
+    String title;
+    double price;
+    String description;
+    String author;
+    int isbn;
+    List <String> type = new ArrayList<String>();
+    int year;
+    int pages;
+    int review_id;
+    int num_of_copies_for_buy;
+    int num_of_copies_for_lending;
+
+    //List<AvailabilityList> availabilitylist = new ArrayList<AvailabilityList>();
+
+
+    Book(){
+
+    }
+    public Book(String ttl, String aut, int is, List <String> ty, int ye){
+
+        title = ttl;
+        author = aut;
+        isbn = is;
+        type = ty;
+        year = ye;
+    }
+
+    public boolean checkBookData(){
+
+        if(String.valueOf(year).length() != 4)
+            throw new IllegalArgumentException("Error...");
+        if(String.valueOf(title).length() < 2 | title == null)
+            throw new IllegalArgumentException("Error...");
+        if(author == null)
+            throw new IllegalArgumentException("Error...");
+        if(type.isEmpty())
+            throw new IllegalArgumentException("Error...");
+        if(String.valueOf(isbn).length() < 8  )
+            throw new IllegalArgumentException("Error...");
+
+
+        return true;
+    }
+
+    public int getId(){
+
+        return  id;
+    }
+
+    public List<String> getTypes(){
+
+        return type;
+    }
+
+    public double getprice(){
+
+        return price;
+    }
+
+    List<BookList> book_list = new ArrayList<BookList>();
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public List<BookList> getBook(String type) throws IOException {//todo θελει αλλαγη
+        List<BookList> returnList = new ArrayList<BookList>();
+
+        for(int i=0; i<book_list.size(); i++){
+
+            if(book_list.get(i).type == type) returnList.add(book_list.get(i));//book_list.get(i).type == type
+        }
+
+        return returnList;
+    }
+
+    public boolean checkAvailability(){
+
+        boolean availability_status = false;//book_not_available()
+
+        //for (int i=0; i<books.size(); i++) {
+        // Book randbook = (Book) books.get(i);
+
+        if (num_of_copies_for_lending > 0) { //randbook.availability == 1
+
+            availability_status = true;//book_available()
+
+        }
+        //}
+        return availability_status;
+    }
+
+    public int stock(){
+
+        return num_of_copies_for_buy;
+    }
+
+    public String checkbookstatus(){
+
+        int status = 0;
+        String message;
+
+        if(num_of_copies_for_lending > 0 & num_of_copies_for_buy < 0)
+            message="daneismos";
+        else if(num_of_copies_for_lending < 0 & num_of_copies_for_buy > 0)
+            message="agora";
+        else if(num_of_copies_for_lending > 0 & num_of_copies_for_buy > 0)
+            message="daneismos kai agora";
+        else
+            message="oxi diathesimo";
+
+
+        return message;
+    }
+
+
+}
+
+
+
+
---
+++ app/src/main/java/com/example/Bookworming/User.java
@@ -0,0 +1,199 @@
+package com.example.bookworms;
+
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+//public @RequiresApi(api = Build.VERSION_CODES.O)
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class User{
+
+    String name;
+    String email;
+    String password;
+    int points;
+    private int id;
+
+
+    //MyBooks list
+    List<MyBooks> my_books = new ArrayList<MyBooks>();//todo read from file
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public User(String na, String em, String pass) throws IOException {
+
+        name = na;
+        email = em;
+        password = pass;
+
+        //List<BorrowedBooks> books = new ArrayList<BorrowedBooks>();
+        //book2.book_id = 2;
+        //book3.book_id = 3;
+        //books.add(book2);
+        //books.add(book3);
+        //bookCheck();
+
+        Log.d("myTag", "User created");
+    }
+
+    public void clickOnCompareBook(){//todo -> or clickOnCompareButton
+
+    }
+
+    public int getPoints(){
+
+        return points;
+    }
+
+    /*public void setScoreOfBook(Book b){
+
+        Reviews review = new Reviews();
+        addReview(stars);
+    }
+
+    public void addReview(){
+
+
+    }
+
+    public void reviewBook(Book b){
+
+        Reviews review = new Reviews();
+
+    }*/
+
+    //CompareBook list
+    List<CompareBook> compare_books = new ArrayList<CompareBook>();//todo read from file
+    int[] compare = new int[2];
+
+    int k = 0;
+    public void chooseBookToCompare(int cid){
+
+        if(k<1) {
+            compare[k] = cid;
+            k++;
+        }
+        else getAnotherBookToCompare(cid);
+
+        //new CompareBook(id, bid);
+    }
+
+    public void getAnotherBookToCompare(int c2id){
+
+        if(k<2){
+            compare[k] = c2id;
+            CompareBook bcompare = new CompareBook(id, compare[0], compare[1]);
+            //compare_books.add(rndbook);//todo
+            bcompare.saveOnDatabase();
+        }
+
+    }
+
+    public void setName(String new_name){
+
+        name = new_name;
+    }
+
+    public String getName(){
+
+        return name;
+    }
+
+    public String getEmail(){
+
+        return email;
+    }
+
+    public String getPassword(){
+
+        return password;
+    }
+
+    public void setPassword(String new_password){
+
+        password = new_password;
+
+    }
+    public void addBookData(Book b){
+
+       if(b.checkBookData()){
+            dataOk();
+       }
+       else dataNotOk();
+    }
+
+    public void dataOk(){
+        System.out.println("Completion message");//todo
+        addPoints(5);
+    }
+
+    public void dataNotOk(){
+        System.out.println("Error");
+    }
+
+
+    //BorrowedBooks list
+    List<BorrowedBooks> b_books = new ArrayList<BorrowedBooks>();//todo read from file
+    //create
+    BorrowedBooks book2 = new BorrowedBooks();
+    BorrowedBooks book3 = new BorrowedBooks();
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public boolean bookCheck(int id) throws IOException {
+
+        Log.d("booker2", "bookCheck message");
+
+        //CSVReader reader = new CSVReader(new FileReader("raw/book.csv"));
+        //List books = reader.readAll();
+        //int size = books.size();
+
+        boolean message = false;//notFindBook()
+
+        //Log.d("MainActivity", String.valueOf(message));
+        //print(a);
+        for (int i=0; i<b_books.size(); i++) {
+            BorrowedBooks randbook = (BorrowedBooks) b_books.get(i);
+            Log.d("booker", String.valueOf(randbook.book_id)); //String.valueOf(randbook.book_id)
+            if ( randbook.book_id == id) {
+                //return (int) books.get(0);//findBook()
+                message = true;//findBook()
+
+                checkDate(randbook.book_id);
+            }
+        }
+        return message;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public boolean checkDate(int id) throws IOException {
+
+        //CSVReader reader = new CSVReader(new FileReader("Book.csv"));
+        //List books = reader.readAll();
+        //int size = books.size();
+
+        boolean message = false;//FindBookNotXreosi()
+
+        for (int i=0; i<b_books.size(); i++) {
+            BorrowedBooks randbook = (BorrowedBooks) b_books.get(i);
+            if (randbook.book_id == id) {
+                //compare book date with current
+
+                if (randbook.dayscalculation() > 20)
+
+                    //return (int) books.get(0);//findBook()
+                    message = true;//findBookXreosi()
+            }
+        }
+        return message;
+
+    }
+
+    public void addPoints(int newpoints){
+
+        points += newpoints;
+    }
+}
