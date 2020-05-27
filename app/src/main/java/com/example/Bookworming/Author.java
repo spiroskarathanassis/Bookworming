4cdca06fff4747092f5a5dfa81ac2c2bcd9ef69d
georgioynikh <65353848+georgioynikh@users.noreply.github.com>
2020-05-27 17:12:36 +0300

    Author class commit

---
+++ app/src/main/java/com/example/Bookworming/Author.java
@@ -0,0 +1,19 @@
+
+public class Author {
+	private String name;
+	private char sexual;
+	
+	public Author(String name, char sexual) {
+		this.name = name;
+		this.sexual = sexual;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public char getSexual() {
+		return sexual;
+	}
+	
+}
